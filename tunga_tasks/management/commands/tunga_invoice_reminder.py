87628d472881e04470f994e21193b53226056b3b
Wangolo Joel <wangoloj@aol.com>
2018-03-02 16:19:57 +0300

    Added the template for rendering reminders to client about invoices.

--- tunga_tasks/management/commands/tunga_invoice_reminder.py
+++ tunga_tasks/management/commands/tunga_invoice_reminder.py
@@ -1,7 +1,7 @@
 import datetime
 from django.core.management.base import BaseCommand
 from tunga_tasks.models import Task
-from tunga_tasks.notifications.email import notify_new_task_invoice_admin_email
+from tunga_tasks.notifications.email import notify_new_task_invoice_admin_email, notify_new_task_invoice_client_email
 
 class Command(BaseCommand):
 
@@ -16,9 +16,10 @@ class Command(BaseCommand):
         for tsk in all_tasks:
             if(tsk.is_due_date(reminder_day)):
                 # Send a Email or slack.
-                notify_new_task_invoice_admin_email(tsk.invoice,
-                                        recipient=tsk.invoice.client.email,
-                                        reminder=True, client=True
+                #notify_new_task_invoice_client_email(instance, recipient=None, reminder=False, client=False, reminder_template="")
+                notify_new_task_invoice_client_email(tsk.invoice,
+                                        reminder=True, client=True,
+                                        reminder_template="70-invoice-reminder-1"
                                         )
             else:
                 print "Not yet due date"
