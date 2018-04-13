b8acf02f5e9a8e81efad65c8a573022788260872
david <tdsemakula@gmail.com>
2018-04-13 07:24:33 +0300

    auto fill created by for site meta

--- tunga_utils/admin.py
+++ tunga_utils/admin.py
@@ -48,7 +48,7 @@ class ContactRequestAdmin(admin.ModelAdmin):
 
 
 @admin.register(SiteMeta)
-class SiteMetaAdmin(admin.ModelAdmin):
+class SiteMetaAdmin(AdminAutoCreatedBy):
     list_display = ('meta_key', 'meta_value', 'created_at')
     list_filter = ('created_at',)
     search_fields = ('meta_key',)
