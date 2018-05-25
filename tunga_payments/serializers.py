7eeb5d6440b6757c09983d281d1f95a8c8da88b8
Remo Samuel Paul <remosam42@gmail.com>
2018-05-25 15:18:47 +0300

    fixing serializer

--- tunga_payments/serializers.py
+++ tunga_payments/serializers.py
@@ -6,12 +6,12 @@ from tunga_payments.models import Payment, Invoice
 class InvoiceSerializer(ModelSerializer):
     class Meta:
         model = Invoice
-        fields = ('project', 'user', 'type', 'amount', 'currency', 'tax_rate',
+        fields = ('id', 'project', 'user', 'type', 'amount', 'currency', 'tax_rate',
                   'processing_fee', 'created_by', 'number', 'batch_ref')
 
 
 class PaymentSerializer(ModelSerializer):
     class Meta:
         model = Payment
-        fields = ('project', 'invoice', 'amount', 'currency', 'payment_method',
+        fields = ('id', 'invoice', 'amount', 'currency', 'payment_method',
                   'created_by')
