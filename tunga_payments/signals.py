8dcb6a2a9ba067b7e97d4d2b7973a9330c0fe52c
david <tdsemakula@gmail.com>
2018-07-05 16:30:24 +0300

    fix v3 signals

--- tunga_payments/signals.py
+++ tunga_payments/signals.py
@@ -7,12 +7,12 @@ from tunga_payments.models import Invoice, Payment
 
 
 @receiver(post_save, sender=Invoice)
-def activity_handler_new_task(sender, instance, created, **kwargs):
+def activity_handler_new_invoice(sender, instance, created, **kwargs):
     if created:
         action.send(instance.created_by, verb=verbs.CREATE, action_object=instance)
 
 
 @receiver(post_save, sender=Payment)
-def activity_handler_new_task(sender, instance, created, **kwargs):
+def activity_handler_new_payment(sender, instance, created, **kwargs):
     if created:
         action.send(instance.created_by, verb=verbs.CREATE, action_object=instance)
--- tunga_projects/signals.py
+++ tunga_projects/signals.py
@@ -7,24 +7,24 @@ from tunga_projects.models import Project, Participation, Document, ProgressEven
 
 
 @receiver(post_save, sender=Project)
-def activity_handler_new_task(sender, instance, created, **kwargs):
+def activity_handler_new_project(sender, instance, created, **kwargs):
     if created:
         action.send(instance.user, verb=verbs.CREATE, action_object=instance)
 
 
 @receiver(post_save, sender=Participation)
-def activity_handler_new_task(sender, instance, created, **kwargs):
+def activity_handler_new_participation(sender, instance, created, **kwargs):
     if created:
         action.send(instance.created_by, verb=verbs.CREATE, action_object=instance)
 
 
 @receiver(post_save, sender=Document)
-def activity_handler_new_task(sender, instance, created, **kwargs):
+def activity_handler_new_document(sender, instance, created, **kwargs):
     if created:
         action.send(instance.created_by, verb=verbs.CREATE, action_object=instance)
 
 
 @receiver(post_save, sender=ProgressEvent)
-def activity_handler_new_task(sender, instance, created, **kwargs):
+def activity_handler_new_progress_event(sender, instance, created, **kwargs):
     if created:
         action.send(instance.created_by, verb=verbs.CREATE, action_object=instance)
