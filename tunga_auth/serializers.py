987c1dd8aa6c3b765ebe6b58f641b2afce141fe6
david <tdsemakula@gmail.com>
2018-07-11 16:29:57 +0300

    add is_admin to all user serializers

--- tunga_auth/serializers.py
+++ tunga_auth/serializers.py
@@ -32,6 +32,7 @@ class UserSerializer(SimpleUserSerializer, GetCurrentUserAnnotatedSerializerMixi
     is_developer = serializers.BooleanField(read_only=True, required=False)
     is_project_owner = serializers.BooleanField(read_only=True, required=False)
     is_project_manager = serializers.BooleanField(read_only=True, required=False)
+    is_admin = serializers.BooleanField(read_only=True, required=False)
     profile = SimpleProfileSerializer(read_only=True, required=False)
     company = SimpleCompanySerializer(read_only=True, required=False)
     work = SimpleWorkSerializer(many=True, source='work_set', read_only=True, required=False)
