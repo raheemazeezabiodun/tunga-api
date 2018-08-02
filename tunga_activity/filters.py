2988d5a21d4aa8304c734a4571036359c98c80cb
david <tdsemakula@gmail.com>
2018-08-02 22:05:10 +0300

    add documents to project activity filter

--- tunga_activity/filters.py
+++ tunga_activity/filters.py
@@ -6,7 +6,7 @@ from django.db.models import Q
 
 from tunga_activity import verbs
 from tunga_comments.models import Comment
-from tunga_projects.models import Project, ProgressEvent, ProgressReport, Participation
+from tunga_projects.models import Project, ProgressEvent, ProgressReport, Participation, Document
 from tunga_tasks.models import Task
 from tunga_utils.filters import GenericDateFilterSet
 from tunga_utils.models import Upload
@@ -43,7 +43,7 @@ class ActionFilter(GenericDateFilterSet):
             Q(projects=project) | Q(progress_events__project=project),
             action_object_content_type__in=[
                 ContentType.objects.get_for_model(model) for model in [
-                    Comment, Upload, ProgressEvent, ProgressReport, Participation
+                    Comment, Upload, ProgressEvent, ProgressReport, Participation, Document
                 ]
             ]
         )
