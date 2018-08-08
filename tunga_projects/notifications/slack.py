055230f4a34138e775b48f6b0ec418125ff99cb5
david <tdsemakula@gmail.com>
2018-08-08 08:24:14 +0300

    slack notification fixes

--- tunga_projects/notifications/slack.py
+++ tunga_projects/notifications/slack.py
@@ -1,8 +1,9 @@
+from django.template.defaultfilters import floatformat, truncatewords
 from django_rq import job
 
 from tunga.settings import TUNGA_URL, SLACK_STAFF_INCOMING_WEBHOOK, SLACK_STAFF_LEADS_CHANNEL, \
-    SLACK_ATTACHMENT_COLOR_TUNGA
-from tunga_tasks.models import Project
+    SLACK_ATTACHMENT_COLOR_TUNGA, SLACK_ATTACHMENT_COLOR_GREEN
+from tunga_projects.models import Project
 from tunga_utils import slack_utils
 from tunga_utils.helpers import clean_instance
 
@@ -26,6 +27,26 @@ def notify_new_project_slack_admin(instance):
             slack_utils.KEY_COLOR: SLACK_ATTACHMENT_COLOR_TUNGA
         }
     ]
+
+    extra_details = ''
+    if instance.type:
+        extra_details += '*Type*: {}\n'.format(instance.get_type_display())
+    if instance.expected_duration:
+        extra_details += '*Expected duration*: {}\n'.format(instance.get_type_display())
+    if instance.skills:
+        extra_details += '*Skills*: {}\n'.format(instance.skills_list)
+    if instance.deadline:
+        extra_details += '*Deadline*: {}\n'.format(instance.deadline.strftime("%d %b, %Y"))
+    if instance.budget:
+        amount = instance.budget
+        extra_details += '*Fee*: EUR {}\n'.format(floatformat(amount, arg=-2))
+
+    if extra_details:
+        attachments.append({
+            slack_utils.KEY_TEXT: extra_details,
+            slack_utils.KEY_MRKDWN_IN: [slack_utils.KEY_TEXT],
+            slack_utils.KEY_COLOR: SLACK_ATTACHMENT_COLOR_GREEN
+        })
 
     slack_utils.send_incoming_webhook(SLACK_STAFF_INCOMING_WEBHOOK, {
         slack_utils.KEY_TEXT: summary,
