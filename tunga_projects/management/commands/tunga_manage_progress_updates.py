3b61ddcc79261f0fa73edd89f9872679e3e8046d
david <tdsemakula@gmail.com>
2018-08-11 00:21:34 +0300

    update progress event scheduler and reminder

---
+++ tunga_projects/management/commands/tunga_manage_progress_updates.py
@@ -0,0 +1,81 @@
+import datetime
+
+from django.core.management.base import BaseCommand
+from django.db.models.query_utils import Q
+
+from tunga_projects.models import Project, ProgressEvent
+from tunga_projects.notifications.generic import remind_progress_event
+from tunga_utils.constants import STATUS_ACCEPTED, PROGRESS_EVENT_DEVELOPER, PROGRESS_EVENT_PM, PROGRESS_EVENT_CLIENT, \
+    PROGRESS_EVENT_MILESTONE, PROGRESS_EVENT_INTERNAL
+
+
+class Command(BaseCommand):
+
+    def handle(self, *args, **options):
+        """
+        Schedule progress updates and send update reminders
+        """
+        # command to run: python manage.py tunga_manage_progress_updates
+
+        today_start = datetime.datetime.utcnow().replace(hour=0, minute=0, second=0, microsecond=0)
+        today_noon = datetime.datetime.utcnow().replace(hour=12, minute=0, second=0, microsecond=0)
+        today_end = datetime.datetime.utcnow().replace(hour=23, minute=59, second=59, microsecond=1000)
+
+        projects = Project.objects.filter(
+            Q(deadline__isnull=True) | Q(deadline__gte=today_start), archived=False
+        )
+        for project in projects:
+
+            all_milestones = ProgressEvent.objects.filter(
+                project=project,
+                type__in=[PROGRESS_EVENT_MILESTONE, PROGRESS_EVENT_INTERNAL],
+                due_at__range=[today_start, today_end]
+            )
+
+            general_milestones = []  # For all stakeholders
+            internal_milestones = []  # Only for PMs
+
+            for milestone in all_milestones:
+                # Send reminders for milestones
+                if not milestone.last_reminder_at:
+                    remind_progress_event(milestone.id)
+
+                # Filter milestone types
+                if milestone.type == PROGRESS_EVENT_MILESTONE:
+                    general_milestones.append(milestone)
+                elif milestone.type == PROGRESS_EVENT_INTERNAL:
+                    internal_milestones.append(milestone)
+
+            if not general_milestones:
+                # Automatic updates are only scheduled if no general milestone falls on this date
+
+                weekday = today_noon.weekday()
+
+                participants = project.participation_set.filter(status=STATUS_ACCEPTED, updates_enabled=True)
+                if weekday < 5 and participants:
+                    # Only developer updates btn Monday (0) and Friday (4)
+                    dev_defaults = dict(title='Developer Update')
+                    dev_event, created = ProgressEvent.objects.update_or_create(
+                        project=project, type=PROGRESS_EVENT_DEVELOPER, due_at=today_noon, defaults=dev_defaults
+                    )
+
+                    if not dev_event.last_reminder_at:
+                        remind_progress_event(dev_event.id)
+
+                if weekday in [0, 3] and project.pm and not internal_milestones:
+                    # PM Reports on Monday (0) and Thursday (3)
+                    pm_defaults = dict(title='PM Report')
+                    pm_event, created = ProgressEvent.objects.update_or_create(
+                        project=project, type=PROGRESS_EVENT_PM, due_at=today_noon, defaults=pm_defaults
+                    )
+                    if not pm_event.last_reminder_at:
+                        remind_progress_event(pm_event.id)
+
+                if weekday == 0:
+                    # Client surveys on Monday (0)
+                    client_defaults = dict(title='Client Survey')
+                    client_event, created = ProgressEvent.objects.update_or_create(
+                        project=project, type=PROGRESS_EVENT_CLIENT, due_at=today_noon, defaults=client_defaults
+                    )
+                    if not client_event.last_reminder_at:
+                        remind_progress_event(client_event.id)
