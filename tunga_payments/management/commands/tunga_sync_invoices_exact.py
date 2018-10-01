9b82f74a616b65e74d374cb9d82bef296148fcb9
david <tdsemakula@gmail.com>
2018-10-01 11:27:56 +0300

    update exact invoice sync

---
+++ tunga_payments/management/commands/tunga_sync_invoices_exact.py
@@ -0,0 +1,27 @@
+import datetime
+
+from dateutil.relativedelta import relativedelta
+from django.core.management.base import BaseCommand
+
+from tunga_payments.models import Invoice
+from tunga_utils.constants import INVOICE_TYPE_SALE, INVOICE_TYPE_PURCHASE
+from tunga_utils.exact_utils import upload_invoice_v3
+
+
+class Command(BaseCommand):
+
+    def handle(self, *args, **options):
+        """
+        Schedule progress updates and send update reminders
+        """
+        # command to run: python manage.py tunga_sync_invoices_exact
+
+        today_start = datetime.datetime.utcnow().replace(hour=0, minute=0, second=0, microsecond=0)
+        past_by_24_hours = today_start - relativedelta(days=24)
+
+        invoices = Invoice.objects.filter(
+            type__in=[INVOICE_TYPE_SALE, INVOICE_TYPE_PURCHASE], paid=True, legacy_id__isnull=True,
+            paid_at__gte=past_by_24_hours
+        )
+        for invoice in invoices:
+            upload_invoice_v3(invoice)
