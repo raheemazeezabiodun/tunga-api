df6cf70bee08857b48397be14712dac839504fb6
david <tdsemakula@gmail.com>
2018-10-23 10:03:30 +0300

    invoice and payment admin updates

--- tunga_payments/admin.py
+++ tunga_payments/admin.py
@@ -3,8 +3,22 @@ from __future__ import unicode_literals
 
 from django.contrib import admin
 
-# Register your models here.
 from tunga_payments.models import Payment, Invoice
+from tunga_utils.admin import ReadOnlyModelAdmin
 
-admin.site.register(Invoice)
-admin.site.register(Payment)
+
+@admin.register(Invoice)
+class InvoiceAdmin(ReadOnlyModelAdmin):
+    list_display = (
+        'title', 'type', 'amount', 'milestone', 'issued_at', 'number', 'tax_rate',
+        'status', 'paid', 'paid_at', 'archived'
+    )
+    list_filter = ('type', 'archived', 'paid')
+    search_fields = ('project__title', 'title', 'number', 'batch_ref')
+
+
+@admin.register(Payment)
+class InvoiceAdmin(ReadOnlyModelAdmin):
+    list_display = ('invoice', 'amount', 'payment_method', 'status', 'paid_at')
+    list_filter = ('status',)
+    search_fields = ('invoice__title',)
