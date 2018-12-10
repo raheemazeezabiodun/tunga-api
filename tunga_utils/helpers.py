dd6f0d13a5a2eb5516eef45fabf757640e21aa9f
Raheem Azeez Abiodun <azeez@aksumlabs.com>
2018-12-10 21:16:14 +0100

    credentials path

--- tunga_utils/helpers.py
+++ tunga_utils/helpers.py
@@ -1,6 +1,7 @@
 import json
 import re
 from decimal import Decimal
+import os
 
 import slackdown
 from allauth.socialaccount.models import SocialToken
@@ -163,7 +164,8 @@ def save_to_google_sheet(url, index, data):
     # use creds to create a client to interact with the Google Drive API
     scope = ['https://spreadsheets.google.com/feeds',
             'https://www.googleapis.com/auth/drive']
-    creds = ServiceAccountCredentials.from_json_keyfile_name('../credentials.json', scope)
+            
+    creds = ServiceAccountCredentials.from_json_keyfile_name(os.path.join(settings.BASE_DIR, 'credentials.json'), scope)
     client = gspread.authorize(creds)
 
     # Make sure you use the right name here.
