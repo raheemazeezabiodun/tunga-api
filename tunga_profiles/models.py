1f6ac840970b05b19bfa4f342dbfb95e0dff69cf
Raheem Azeez Abiodun <azeez@aksumlabs.com>
2018-12-10 17:40:31 +0100

    syntax errors

--- tunga_profiles/models.py
+++ tunga_profiles/models.py
@@ -487,13 +487,13 @@ class UserRequest(models.Model):
 
 @python_2_unicode_compatible
 class WhitePaperUser(models.Model):
-    first_name = models.CharField(nax_length=100)
-    last_name = models.CharField(nax_length=100)
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
     company = models.CharField(max_length=100)
     email = models.EmailField(max_length=100)
     phone_number = models.CharField(max_length=20)
     country = CountryField()
     created_at = models.DateTimeField(auto_now_add=True)
 
-     def __str__(self):
+    def __str__(self):
         return '{} {} - {}'.format(self.first_name, self.email, self.company)
