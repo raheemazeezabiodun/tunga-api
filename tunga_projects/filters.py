1101abaafbd721b5221a337065b518db3c10e13a
Raheem Azeez Abiodun <raheemazeez4@gmail.com>
2019-02-25 11:30:22 +0100

    interest polls

--- tunga_projects/filters.py
+++ tunga_projects/filters.py
@@ -7,14 +7,12 @@ from tunga_utils.filters import GenericDateFilterSet
 
 class ProjectFilter(GenericDateFilterSet):
     participant = django_filters.NumberFilter(name='participants__user', label='Participant')
-    skill = django_filters.CharFilter(name='skills__name', label='skills')
-    skill_id = django_filters.NumberFilter(name='skills', label='skills (by ID)')
     owner = django_filters.CharFilter(method='filter_owner')
 
     class Meta:
         model = Project
         fields = (
-            'user', 'type', 'expected_duration', 'stage', 'participant', 'skill', 'skill_id', 'archived'
+            'user', 'type', 'expected_duration', 'stage', 'participant', 'archived'
         )
 
     def filter_owner(self, queryset, name, value):
